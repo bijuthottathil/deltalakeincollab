c11387da7740d4ae6d24dc4af43de6aaf294e8a2
Biju Mathew Thottathil <63364506+bijuthottathil@users.noreply.github.com>
2022-01-28 14:57:22 -0600

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   client_id       = "ed5d6ac4-de15-4a24-b5d3-61812c9e9941"
   client_secret   = "HOl7Q~23UVXp4J1SlBTsQQSR~dMk2CTsNERWH"
   tenant_id       = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"*/
-   subscription_id =var.subscriptionID
+  subscription_id = var.subscriptionID
   client_id       = var.clientID
   client_secret   = var.clientSecret
   tenant_id       = var.tenantID
