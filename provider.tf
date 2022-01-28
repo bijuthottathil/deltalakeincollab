0c5219f7beaa2a2ee6a677cd8c0b72babd9729ab
Biju Mathew Thottathil <63364506+bijuthottathil@users.noreply.github.com>
2022-01-28 14:29:27 -0600

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.93.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
+  client_id       = "ed5d6ac4-de15-4a24-b5d3-61812c9e9941"
+  client_secret   = "HOl7Q~23UVXp4J1SlBTsQQSR~dMk2CTsNERWH"
+  tenant_id       = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
+  features {}
+}
